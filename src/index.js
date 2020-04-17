dc936c4d013f6c140886aa65127300b6bac22bf2
“Lauren <lucerolaurenm@gmail.com>
2020-04-17 10:17:13 -0600

    Edit html and CSS for login form

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import "./css/base.scss";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/background.png";
 
-console.log("This is the JavaScript entry file - your code begins here.");
-
 // global variables
 let users;
 let rooms;
