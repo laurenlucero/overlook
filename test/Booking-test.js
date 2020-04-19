a9aef37fbb5b7101c55b6f05933f39feee4a39c6
“Lauren <lucerolaurenm@gmail.com>
2020-04-18 19:30:57 -0600

    Complete Booking class and tests

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,17 +1,43 @@
 import chai from "chai";
 const expect = chai.expect;
 
-import sampleData from "./sample-test-data";
+import mockBookingData from "./mock-booking-data";
 import Booking from "../src/Booking";
 
 describe("Booking", function() {
   let booking;
 
   beforeEach(function() {
-    booking = new Booking("5fwrgu4i7k55hl6sz", 1, "2020/02/04", 1);
+    booking = new Booking({
+      id: "5fwrgu4i7k55hl6sz",
+      userID: 9,
+      date: "2020/02/04",
+      roomNumber: 15,
+      roomServiceCharges: []
+    });
   });
 
   it("should be an instance of Booking", function() {
     expect(booking).to.be.an.instanceOf(Booking);
+  });
+
+  it("should return booking id", function() {
+    expect(booking.id).to.equal("5fwrgu4i7k55hl6sz");
+  });
+
+  it("should return the booking userID", function() {
+    expect(booking.userID).to.equal(9);
+  });
+
+  it("should return the booking date", function() {
+    expect(booking.date).to.equal("2020/02/04");
+  });
+
+  it("should return the booking room number", function() {
+    expect(booking.roomNumber).to.equal(15);
+  });
+
+  it("should return the booking room service charges", function() {
+    expect(booking.roomServiceCharges).to.deep.equal([]);
   });
 });
